8a5e83afbc57ec071200e0781c3d8b4c1644fc95
Will Johnson <william.c1.johnson@gmail.com>
2019-03-20 11:56:00 -0400

    Add ability to build RPM locally in case you don't want to upload to copr

--- copr/create_build.py
+++ copr/create_build.py
@@ -9,6 +9,7 @@ import re
 from copr.v3 import Client, CoprNoResultException
 
 version_regex = r"(\d+\.\d+\.\d+)-(?:\d+\.(\d+)|(\d+))\..+"
+valid_truthy_args = ["TRUE", "True", "true", "t", "T", "Y", "y", "YES", "Yes", "yes"]
 
 
 def update_spec_with_new_release(spec_file):
@@ -18,7 +19,9 @@ def update_spec_with_new_release(spec_file):
 
     release_matches = re.match(r".*# Release Start\nRelease:.*(\d)%{\?dist}\n# Release End.*", spec, re.DOTALL)
     cur_rel = [x for x in release_matches.groups() if x is not None].pop()
+    print("Current release value: {}".format(cur_rel))
     new_rel = "{}.{}".format(int(time.time()), cur_rel)
+    print("New release value: {}".format(new_rel))
 
     return re.sub(
         r".*# Release Start\nRelease:.*(\d)%{\?dist}\n# Release End.*",
@@ -50,6 +53,7 @@ package = os.environ.get("PACKAGE")
 spec = os.environ.get("SPEC")
 srpm_path = os.environ.get("SRPM_PATH", "/tmp/*.src.rpm")
 prod = os.environ.get("PROD", False)
+local_only = os.environ.get("LOCAL_ONLY", False)
 
 subprocess.call(
     ["openssl", "aes-256-cbc", "-K", key, "-iv", iv, "-in", "/tmp/copr-mfl.enc", "-out", "/root/.config/copr", "-d"]
@@ -69,7 +73,8 @@ except CoprNoResultException:
 try:
     p = glob.glob(srpm_path).pop()
 except:
-    if prod not in ["TRUE", "True", "true", "t", "T", "Y", "y", "YES", "Yes", "yes"]:
+    if prod not in valid_truthy_args:
+        print("Development Mode: Updating release to include new epoch.")
         spec_file = get_spec_file()
 
         updated_spec = update_spec_with_new_release(spec_file)
@@ -89,17 +94,23 @@ except:
 
     p = glob.glob(srpm_path).pop()
 
-build = client.build_proxy.create_from_file(owner, project, p)
+if local_only in valid_truthy_args:
+    print("Building the RPM from SRPM Locally.")
+    subprocess.call(["rpmbuild", "--rebuild", p], cwd="/build")
+    rpm = glob.glob("/root/rpmbuild/RPMS/x86_64/*.rpm").pop()
+    subprocess.call(["mv", rpm, "/build"])
+    print("RPM location: /build/{}".format(rpm))
+else:
+    print("Uploading SRPM to Copr.")
+    build = client.build_proxy.create_from_file(owner, project, p)
 
+    while client.build_proxy.get(build.id).state in ["running", "pending", "starting", "importing"]:
+        time.sleep(10)
+        print("{} running. State: {}".format(build.id, client.build_proxy.get(build.id).state))
 
-while client.build_proxy.get(build.id).state in ["running", "pending", "starting", "importing"]:
-    time.sleep(10)
-    print("{} running. State: {}".format(build.id, client.build_proxy.get(build.id).state))
+    final_state = client.build_proxy.get(build.id).state
 
-final_state = client.build_proxy.get(build.id).state
+    print("build {} {}".format(build.id, final_state))
 
-print("build {} {}".format(build.id, final_state))
-
-
-if final_state == "failed":
-    sys.exit(1)
+    if final_state == "failed":
+        sys.exit(1)
