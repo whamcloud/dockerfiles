55167ae2b725f9aed075944781ef4c33b175a06b
Will Johnson <wjohnson@whamcloud.com>
2019-03-21 08:19:55 -0400

    Add possible values to indicate that a build is a production build (#11)
    * Add possible values to indicate that a build is a production build
    
    - provide list of possible truthy values for a production build
    - remove unnecessary environment variables
    
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>
    
    * - Update regex to account for a static build on a dev project
    
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>
    
    * Remove release value and instead just use epoch
    
    * Set release format to epoch.rel
    
    * Add ability to build RPM locally in case you don't want to upload to copr
    
    * Small update
    
    * More updates
    
    * Don't limit the rpm path to x86_64.
    
    * Updates
    
    * Update how the RPM is build locally such that the _top directory is included.

--- copr/create_build.py
+++ copr/create_build.py
@@ -7,49 +7,25 @@ import sys
 import glob
 import re
 from copr.v3 import Client, CoprNoResultException
-from packaging import version
 
-version_regex = r"(\d+\.\d+\.\d)+-\d+\.(\d+)\..+"
+release_regex = r".*# Release Start\nRelease:\s*(?:(\d+)|\d+\.(\d+))%{\?dist}\n# Release End.*"
+valid_truthy_args = ["TRUE", "True", "true", "t", "T", "Y", "y", "YES", "Yes", "yes"]
 
 
-def get_version_from_spec(spec_file):
+def update_spec_with_new_release(spec_file):
     file = open(spec_file, "r")
     spec = file.read()
     file.close()
 
-    return re.match(r".*Version:.*(\d+\.\d+\.\d+).*", spec, re.DOTALL).group(1)
-
-
-def update_spec_with_new_release(spec_file, release_num):
-    file = open(spec_file, "r")
-    spec = file.read()
-    file.close()
+    release_matches = re.match(release_regex, spec, re.DOTALL)
+    cur_rel = [x for x in release_matches.groups() if x is not None].pop()
+    print("Current release value: {}".format(cur_rel))
+    new_rel = "{}.{}".format(int(time.time()), cur_rel)
+    print("New release value: {}".format(new_rel))
 
     return re.sub(
-        r".*# Release Start\nRelease:.*(\d)%{\?dist}\n# Release End.*",
-        "# Release Start\nRelease:    {}{}\n# Release End".format(release_num, "%{?dist}"),
-        spec,
-        re.DOTALL,
+        release_regex, "# Release Start\nRelease:    {}{}\n# Release End".format(new_rel, "%{?dist}"), spec, re.DOTALL
     )
-
-
-def get_release_num(builds, version_regex, spec_file):
-    release_num = 1
-
-    try:
-        build = sorted(builds, key=lambda x: x.ended_on, reverse=True)[0]
-        v = build.source_package.get("version")
-        matches = re.match(version_regex, v)
-        ver = matches.group(1)
-
-        spec_ver = get_version_from_spec(spec_file)
-
-        if version.parse(ver) == version.parse(spec_ver):
-            release_num = int(matches.group(2)) + 1
-    except Exception:
-        print("Initial build.")
-
-    return release_num
 
 
 def get_spec_file():
@@ -65,44 +41,25 @@ def write_new_spec(spec_file, new_data):
     file.close()
 
 
-key = os.environ["KEY"]
-iv = os.environ["IV"]
+key = os.environ.get("KEY", "")
+iv = os.environ.get("IV", "")
 
-owner = os.environ["OWNER"]
-project = os.environ["PROJECT"]
-package = os.environ.get("PACKAGE")
-clone_url = os.environ.get("CLONE_URL")
+owner = os.environ.get("OWNER", "")
+project = os.environ.get("PROJECT", "")
+package = os.environ.get("PACKAGE", "")
 spec = os.environ.get("SPEC")
-ish = os.environ.get("COMMITISH", "master")
 srpm_path = os.environ.get("SRPM_PATH", "/tmp/*.src.rpm")
-project_dirname = os.environ.get("PROJECT_DIRNAME")
 prod = os.environ.get("PROD", False)
-
-subprocess.call(
-    ["openssl", "aes-256-cbc", "-K", key, "-iv", iv, "-in", "/tmp/copr-mfl.enc", "-out", "/root/.config/copr", "-d"]
-)
-
-client = Client.create_from_config_file()
-
-args = (owner, project, package)
-
-try:
-    client.project_proxy.get(owner, project)
-except CoprNoResultException:
-    print("project {}/{} not found. Creating it.".format(owner, project))
-    client.project_proxy.add(owner, project, ["epel-7-x86_64"])
-
+local_only = os.environ.get("LOCAL_ONLY", False)
 
 try:
     p = glob.glob(srpm_path).pop()
 except Exception:
-    if not prod:
+    if prod not in valid_truthy_args:
+        print("Development Mode: Updating release to include new epoch.")
         spec_file = get_spec_file()
-        epoch = int(time.time())
-        builds = client.build_proxy.get_list(*args, status="succeeded")
-        release_num = get_release_num(builds, version_regex, spec_file)
 
-        updated_spec = update_spec_with_new_release(spec_file, "{}.{}".format(epoch, release_num))
+        updated_spec = update_spec_with_new_release(spec_file)
         write_new_spec(spec_file, updated_spec)
 
     # Build the SRPM
@@ -119,17 +76,35 @@ except Exception:
 
     p = glob.glob(srpm_path).pop()
 
-build = client.build_proxy.create_from_file(owner, project, p)
+if local_only in valid_truthy_args:
+    print("Building the RPM's from SRPM Locally.")
+    subprocess.call(["rpmbuild", "--rebuild", p, "-D", "%_topdir /build/_topdir"], cwd="/build")
+    print("RPM's located under: /build/_topdir/RPMS")
+else:
+    subprocess.call(
+        ["openssl", "aes-256-cbc", "-K", key, "-iv", iv, "-in", "/tmp/copr-mfl.enc", "-out", "/root/.config/copr", "-d"]
+    )
 
+    client = Client.create_from_config_file()
 
-while client.build_proxy.get(build.id).state in ["running", "pending", "starting", "importing"]:
-    time.sleep(10)
-    print("{} running. State: {}".format(build.id, client.build_proxy.get(build.id).state))
+    args = (owner, project, package)
 
-final_state = client.build_proxy.get(build.id).state
+    try:
+        client.project_proxy.get(owner, project)
+    except CoprNoResultException:
+        print("project {}/{} not found. Creating it.".format(owner, project))
+        client.project_proxy.add(owner, project, ["epel-7-x86_64"])
 
-print("build {} {}".format(build.id, final_state))
+    print("Uploading SRPM to Copr.")
+    build = client.build_proxy.create_from_file(owner, project, p)
 
+    while client.build_proxy.get(build.id).state in ["running", "pending", "starting", "importing"]:
+        time.sleep(10)
+        print("{} running. State: {}".format(build.id, client.build_proxy.get(build.id).state))
 
-if final_state == "failed":
-    sys.exit(1)
+    final_state = client.build_proxy.get(build.id).state
+
+    print("build {} {}".format(build.id, final_state))
+
+    if final_state == "failed":
+        sys.exit(1)
