5717a2a951a6e88eea1f3aabdd694120f9b1b6ab
Joe Grund <jgrund@whamcloud.io>
2020-10-20 12:46:06 -0400

    bump version
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- release-grabber/build-deps.sh
+++ release-grabber/build-deps.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-: "${VERSION:=6.1.0}"
+: "${VERSION:=6.2.0}"
 : "${LUSTRE_VERSION:=2.12.4}"
 
 
@@ -11,16 +11,16 @@ cd /tmp/iml-build
 
 
 if [[ -z "$MANAGER_ONLY" && ! -z "$WITH_LUSTRE_PATCHLESS" ]]; then
-  echo "Adding e2fsprogs repo"
+    echo "Adding e2fsprogs repo"
 
-  yum-config-manager --add-repo=https://downloads.whamcloud.com/public/e2fsprogs/latest/el7/
+    yum-config-manager --add-repo=https://downloads.whamcloud.com/public/e2fsprogs/latest/el7/
 
-  echo "Adding Patchless Lustre repos"
+    echo "Adding Patchless Lustre repos"
 
-  yum-config-manager --add-repo=https://downloads.whamcloud.com/public/lustre/lustre-${LUSTRE_VERSION}/el7/patchless-ldiskfs-server/
-  yum-config-manager --add-repo=http://download.zfsonlinux.org/epel/7.6/kmod/x86_64/
+    yum-config-manager --add-repo=https://downloads.whamcloud.com/public/lustre/lustre-${LUSTRE_VERSION}/el7/patchless-ldiskfs-server/
+    yum-config-manager --add-repo=http://download.zfsonlinux.org/epel/7.6/kmod/x86_64/
 
-  yumdownloader --resolve -y pcs fence-agents fence-agents-virsh lustre-resource-agents lustre-ldiskfs-zfs-patchles
+    yumdownloader --resolve -y pcs fence-agents fence-agents-virsh lustre-resource-agents lustre-ldiskfs-zfs-patchles
 fi
 
 echo "Adding IML manager repos"
@@ -29,7 +29,7 @@ yum-config-manager --add-repo=https://github.com/whamcloud/integrated-manager-fo
 yumdownloader --resolve -y python2-iml-manager
 
 if [[ -z "$MANAGER_ONLY" ]]; then
-  yumdownloader --resolve -y python2-iml-agent python2-iml-agent-management rust-iml-agent;
+    yumdownloader --resolve -y python2-iml-agent python2-iml-agent-management rust-iml-agent;
 fi
 
 createrepo --pretty ./
